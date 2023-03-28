234ccb94bd09c0f2bea6416e46dc43af14712835
diogo <diogoduarte@gmail.com>
2023-03-28 08:05:16 +0200

    database changes

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg');
 
 const client = new Client ({
-    host: "localhost",
-    user: "diogoduarte",
-    port: 5432,
-    password: "mydatabase123",
-    database: "ipostapplication"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "diogoduarte",
+    port: process.env.DB_PORT || 5432,
+    password: process.env.DB_PW || "mydatabase123",
+    database: process.env.DB_DB || "ipostapplication"
 })
 
 module.exports = client;
