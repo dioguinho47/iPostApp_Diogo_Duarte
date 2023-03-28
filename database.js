eca2c61293520448087cfacb5ccded86286a9dea
diogo <diogoduarte@gmail.com>
2023-03-28 09:29:12 +0200

    heroku test 2

--- database.js
+++ database.js
@@ -5,7 +5,10 @@ const client = new Client ({
     user: process.env.DB_USER || "diogoduarte",
     port: process.env.DB_PORT || 5432,
     password: process.env.DB_PW || "mydatabase123",
-    database: process.env.DB_DB || "ipostapplication"
+    database: process.env.DB_DB || "ipostapplication",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = client;
